bd3d0b7b10439b7f7dea299fa5a34915b2f42b47
Gabriel Almeida Dias <bielalmeidadias@hotmail.com>
2025-04-28 14:30:20 -0300

    Fazendo o cronometro e o footer

---
+++ app/index.jsx
@@ -0,0 +1,74 @@
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={require("./pomodoro.png")} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>
+          25:00
+        </Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício para estudo de React Native.
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Gabriel.
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 40,
+  },
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#B872FF",
+    borderRadius: 32,
+    padding: 8,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+  },
+  footer: {
+
+    width: "80%",
+  },
+  footerText: {
+    textAlign: "center",
+    color: "#98A0A8",
+    fontSize: 12.5,
+  }
+});
